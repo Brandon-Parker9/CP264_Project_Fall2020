c36383a5c7e46db61707a5b3ba889f1916e55bf9
Brandon-Parker9 <brandonparker119@gmail.com>
2020-11-19 17:30:20 -0500

    First Upload
    created a new project, now I am puttig it on GitHub

---
+++ CP264_Final_project/src/main.c
@@ -0,0 +1,25 @@
+/*
+ -------------------------------------
+ File:    main.c
+ Project: CP264_Final_project
+ file description
+ -------------------------------------
+ Author:  Brandon Parker
+ ID:      191593730
+ Email:   park3730@mylaurier.ca
+ Version  2020-11-19
+ -------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	setbuf(stdin, NULL);
+
+	printf("Hello World!");
+
+	return 0;
+
+}
