64ecb8be501c74e2ea6c1b7eb5e87c14dab8ae80
Brandon-Parker9 <brandonparker119@gmail.com>
2020-11-19 20:49:38 -0500

    Minor changes

--- CP264_Final_project/src/character_manipulation.c
+++ CP264_Final_project/src/character_manipulation.c
@@ -10,9 +10,28 @@
  Version  2020-11-19
  -------------------------------------
  */
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "character_manipulation.h"
 
+int frequency_array[255] = { 0 };
+
 void string_to_array(char *string) {
-	printf("String length: %d", strlen(string));
+	printf("String length: %ld\n", strlen(string));
+
+	for (int i = 0; i < strlen(string); i++) {
+		printf("Character: %c\n", string[i]);
+		frequency_array[(int) string[i]] = frequency_array[(int) string[i]] + 1;
+
+	}
+
+	for (int i = 0; i < 255; i++) {
+		if (frequency_array[i] != 0) {
+			printf("char: %c freq: %d\n", ((char) i), frequency_array[i]);
+		}
+
+	}
 }
+
