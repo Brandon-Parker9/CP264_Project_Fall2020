4557393a3bb65d4d2562ac19001792bd10c0c0e5
Brandon-Parker9 <brandonparker119@gmail.com>
2020-11-19 21:07:00 -0500

    Create node_struct.h

---
+++ CP264_Final_project/src/node_struct.h
@@ -0,0 +1,18 @@
+/*
+-------------------------------------
+File:    node_struct.h
+Project: CP264_Final_project
+file description
+-------------------------------------
+Author:  Brandon Parker
+ID:      191593730
+Email:   park3730@mylaurier.ca
+Version  2020-11-19
+-------------------------------------
+ */
+#ifndef NODE_STRUCT_H_
+#define NODE_STRUCT_H_
+
+
+
+#endif /* NODE_STRUCT_H_ */
