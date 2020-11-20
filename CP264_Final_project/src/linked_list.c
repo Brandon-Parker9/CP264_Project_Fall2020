85d16265271ee97f6371690454890c246259c7fe
Brandon-Parker9 <brandonparker119@gmail.com>
2020-11-19 21:06:57 -0500

    Create linked_list.c

---
+++ CP264_Final_project/src/linked_list.c
@@ -0,0 +1,13 @@
+/*
+-------------------------------------
+File:    linked_list.c
+Project: CP264_Final_project
+file description
+-------------------------------------
+Author:  Brandon Parker
+ID:      191593730
+Email:   park3730@mylaurier.ca
+Version  2020-11-19
+-------------------------------------
+ */
+
