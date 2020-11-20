f659b22308f6022a79964d86bd4c14030678d88d
Brandon-Parker9 <brandonparker119@gmail.com>
2020-11-19 19:42:45 -0500

    Create character_manipulation.c

---
+++ CP264_Final_project/src/character_manipulation.c
@@ -0,0 +1,19 @@
+/*
+ -------------------------------------
+ File:    character_manipulation.c
+ Project: CP264_Final_project
+ file description
+ -------------------------------------
+ Author:  Brandon Parker
+ ID:      191593730
+ Email:   park3730@mylaurier.ca
+ Version  2020-11-19
+ -------------------------------------
+ */
+
+typedef struct char_item {
+	char character;
+	int frequency;
+
+} char_item;
+
