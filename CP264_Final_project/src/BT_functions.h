d225e6abded29cfd42382d98e013f0094915336b
Brandon-Parker9 <brandonparker119@gmail.com>
2020-11-22 19:32:46 -0500

    Create BT_functions.h

---
+++ CP264_Final_project/src/BT_functions.h
@@ -0,0 +1,18 @@
+/*
+ -------------------------------------
+ File:    BST_functions.h
+ Project: CP264_Final_project
+ file description
+ -------------------------------------
+ Author:  Brandon Parker
+ ID:      191593730
+ Email:   park3730@mylaurier.ca
+ Version  2020-11-22
+ -------------------------------------
+ */
+#ifndef BT_FUNCTIONS_H_
+#define BT_FUNCTIONS_H_
+
+void search_binary_tree(node *root, char character, int *left, int *right);
+
+#endif /* BT_FUNCTIONS_H_ */
