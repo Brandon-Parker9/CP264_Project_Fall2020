ada5f8d914ec006cafa9e6add172fef86a27235c
notWilll <74434050+notWilll@users.noreply.github.com>
2020-11-28 19:25:08 -0500

    Update decode.c

--- CP264_Final_project/src/decode.c
+++ CP264_Final_project/src/decode.c
@@ -4,7 +4,7 @@
  Project: mabi3240_Quiz3
  file description
  -------------------------------------
- Author:  William Mabia
+ Author:  William Mabia, Tobi Onabolu
  ID:      190133240
  Email:   mabi3240@mylaurier.ca
  Version  2020-09-18
@@ -17,8 +17,43 @@
 #include <string.h>
 #include "decode.h"
 
-#include "character_manipulation.h"
 #include "node_struct.h"
-#include "linked_list.h"
 
-//In progress....
+void getFile(node *root){
+//    Have a file pointer pointing to each character in the file
+//    Have a while loop to makes sure that the current._next is not NULL(Want to stop at leaf, leaf = letter)
+//    For each character if its a 1 traverse right tree
+//    if its a 0 traverse left
+//    ** out of the loop
+//    Once we hit a leaf print the leaf node.character
+    
+    char letter;
+//    int , length, l = 10, j;
+    FILE *readFile;
+    
+    node *curr = root;
+    
+    
+    readFile = fopen("msgDecrypt", "r");
+    
+    while (!feof(readFile)){
+        fscanf(readFile, "%c", &letter);
+        if(letter == 0 || letter == 1){//Prevent Infinite loop
+            while (curr->left != NULL || curr->right != NULL){
+                if (letter == 0)
+                    curr = curr->left;
+            
+                else if (letter == 1)
+                    curr = curr->right;
+            }
+        }
+        printf("%c", curr->character);
+    
+            
+    }
+//    fgets(&letter, 50, readFile);
+    
+    
+}
+
+
